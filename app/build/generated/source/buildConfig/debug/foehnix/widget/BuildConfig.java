c64c30133b08de0b03993c4a1dd3f39665b41714
dzmanto <dzmanto@hotmail.com>
2017-12-30 18:46:06 +0100

    upload proper .apk

--- app/build/generated/source/buildConfig/debug/foehnix/widget/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/foehnix/widget/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "foehnix.widget";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 22;
-  public static final String VERSION_NAME = "2.2";
+  public static final int VERSION_CODE = 23;
+  public static final String VERSION_NAME = "2.3";
 }
