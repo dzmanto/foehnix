37ee7b4ba001c271672ff459ca4c6225a4c318fe
dzmanto <dzmanto@hotmail.com>
2020-03-24 09:10:53 +0100

    Fixes issue with Scroll... class

--- app/src/main/java/foehnix/widget/MyWidgetProvider.java
+++ app/src/main/java/foehnix/widget/MyWidgetProvider.java
@@ -1,5 +1,4 @@
 package foehnix.widget;
-// todo: thirdstockview marquee via html
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,20 +6,12 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Random;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.Inflater;
 import java.util.zip.InflaterInputStream;
-
-import foehnix.widget.R;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -29,14 +20,9 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import foehnix.widget.ScrollTextView;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.SystemClock;
-import android.preference.PreferenceManager;
 import android.text.Html;
 import android.util.Log;
 import android.widget.RemoteViews;
