d4a3584be1a50091ebfea27084d2557c4fad6e53
dzmanto <dzmanto@hotmail.com>
2023-10-10 17:35:49 +0200

    added an app

--- app/src/main/java/foehnix/widget/MyMainActivity.java
+++ app/src/main/java/foehnix/widget/MyMainActivity.java
@@ -1,6 +1,19 @@
 package foehnix.widget;
 
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView welcomeMessageTV = new TextView(this);
+        welcomeMessageTV.setText(Html.fromHtml("<h2>How to install the föhnix widget<h2><ul><<li> Tap on the home screen</li><li>Select <i>widgets<</i></li><li> Select the föhnix widget from the list</li><li> Arrange the föhnix widget on the screen</li></ul>"));
+        setContentView(welcomeMessageTV);
+    }
+
 }
